d74976edd59b61bf1b3882d87683522045b0ca6c
kralinskyi <98835571+kralinskyi@users.noreply.github.com>
2023-11-29 20:06:46 +0000

    Deploying to gh-pages from @ kralinskyi/goit-hw-08@d5af2ec252193935ac79412cc005e645a33f8e8b 🚀

---
+++ 01-color-switcher.1de4b502.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(){t.setAttribute("disabled","true"),n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){document.body.removeAttribute("style"),t.removeAttribute("disabled","false"),clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.1de4b502.js.map
