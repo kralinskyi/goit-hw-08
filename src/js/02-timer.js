3b082abdef3bd083354209d10df80eac778b89b0
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-11-30 14:20:18 +0200

    add click handle

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,36 +1,3 @@
-/*
-
-
-
-Вибір дати
-Метод onClose() з об'єкта параметрів викликається щоразу під час закриття елемента інтерфейсу, який створює flatpickr. Саме у ньому варто обробляти дату, обрану користувачем. Параметр selectedDates - це масив обраних дат, тому ми беремо перший елемент.
-
-Якщо користувач вибрав дату в минулому, покажи window.alert() з текстом "Please choose a date in the future".
-Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
-Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому.
-Натисканням на кнопку «Start» починається відлік часу до обраної дати з моменту натискання.
-
-ідлік часу
-Натисканням на кнопку «Start» скрипт повинен обчислювати раз на секунду, скільки часу залишилось до вказаної дати, і оновлювати інтерфейс таймера, показуючи чотири цифри: дні, години, хвилини і секунди у форматі xx:xx:xx:xx.
-
-Кількість днів може складатися з більше, ніж двох цифр.
-Таймер повинен зупинятися, коли дійшов до кінцевої дати, тобто 00:00:00:00.
-НЕ БУДЕМО УСКЛАДНЮВАТИ
-Якщо таймер запущений, для того щоб вибрати нову дату і перезапустити його - необхідно перезавантажити сторінку.
-
-Для підрахунку значень використовуй готову функцію convertMs, де ms - різниця між кінцевою і поточною датою в мілісекундах.
-
-Форматування часу
-Функція convertMs() повертає об'єкт з розрахованим часом, що залишився до кінцевої дати. Зверни увагу, що вона не форматує результат. Тобто, якщо залишилося 4 хвилини або будь-якої іншої складової часу, то функція поверне 4, а не 04. В інтерфейсі таймера необхідно додавати 0, якщо в числі менше двох символів. Напиши функцію addLeadingZero(value), яка використовує метод padStart() і перед рендерингом інтефрейсу форматує значення.
-
-Бібліотека повідомлень
-УВАГА
-Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
-
-Для відображення повідомлень користувачеві, замість window.alert(), використовуй бібліотеку notiflix.
-
-*/
-
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix';
@@ -40,28 +7,24 @@ const daysLeft = document.querySelector('[data-days]');
 const hoursLeft = document.querySelector('[data-hours]');
 const minutesLeft = document.querySelector('[data-minutes]');
 const secondsLeft = document.querySelector('[data-seconds]');
+let countdownInterval = null;
 
 const options = {
   enableTime: true,
   time_24hr: true,
-  defaultDate: new Date(),
+  defaultDate: Date.now(),
   minDate: 'today',
   minuteIncrement: 1,
   onClose(selectedDates) {
-    setInterval(() => {
-      const remainingTime = convertMs(selectedDates[0].getTime() - Date.now());
-      if (remainingTime.seconds < 0) {
-        Notify.failure('Shoose time in future tense!');
-        startBtn.disabled = true;
-        return;
-      }
-      startBtn.disabled = false;
-      timeInterfaceDrawning(remainingTime);
-    }, 1000);
+    const remainingTime = convertMs(selectedDates[0].getTime() - Date.now());
+    if (remainingTime.seconds < 0) {
+      Notify.failure('Shoose time in future tense!');
+      startBtn.disabled = true;
+      return;
+    }
+    startBtn.disabled = false;
   },
 };
-
-startBtn.addEventListener('click', onStartBtnClick);
 
 function timeInterfaceDrawning({ days, hours, minutes, seconds }) {
   daysLeft.textContent = days;
@@ -69,8 +32,6 @@ function timeInterfaceDrawning({ days, hours, minutes, seconds }) {
   minutesLeft.textContent = minutes;
   secondsLeft.textContent = seconds;
 }
-
-function onStartBtnClick() {}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -80,15 +41,40 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = addZero(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = addZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = addZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = addZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
 
+function addZero(value) {
+  return String(value).padStart(2, '0');
+}
+
 const date = flatpickr('#datetime-picker', options);
+
+startBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+  countdownInterval = setInterval(updateTimeInterface, 1000);
+}
+
+function updateTimeInterface() {
+  const selectedDate = date.selectedDates[0].getTime();
+  const currentTime = Date.now();
+
+  const remainingTime = convertMs(selectedDate - currentTime);
+
+  if (remainingTime.seconds < 0) {
+    clearInterval(countdownInterval);
+    Notify.failure('Please select a time in the future!');
+    return;
+  }
+
+  timeInterfaceDrawning(remainingTime);
+}
