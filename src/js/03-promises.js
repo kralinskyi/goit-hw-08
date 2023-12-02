d79a7dd7ed243ff7f2ebb20a3f31758f55d7276b
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-12-02 12:56:16 +0200

    better promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,31 +1,8 @@
-/*
-
-иши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount. Під час кожного виклику передай їй номер промісу (position), що створюється, і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
-
-Доповни код функції createPromise таким чином, щоб вона повертала один проміс, який виконується або відхиляється через delay часу. Значенням промісу повинен бути об'єкт, в якому будуть властивості position і delay зі значеннями однойменних параметрів. Використовуй початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-
-  */
-
 import { Notify } from 'notiflix';
 
-document.querySelector('.form').addEventListener('submit', handleSubmitform);
+const form = document
+  .querySelector('.form')
+  .addEventListener('submit', handleSubmitform);
 
 function handleSubmitform(e) {
   e.preventDefault();
@@ -37,16 +14,13 @@ function handleSubmitform(e) {
   let amountNumber = Number(amount.value);
 
   for (let i = 1; i <= amountNumber; i += 1) {
-    createPromise(i, currentDelay)
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
+    createPromise(i, currentDelay).then(onSuccess).catch(onRejected);
 
     currentDelay += delayStep;
   }
+  setTimeout(() => {
+    e.target.reset();
+  }, currentDelay);
 }
 
 function createPromise(position, delay) {
@@ -57,7 +31,15 @@ function createPromise(position, delay) {
       if (shouldResolve) {
         resolve({ position, delay });
       }
+
       reject({ position, delay });
     }, delay);
   });
+}
+
+function onSuccess({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+function onRejected({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
